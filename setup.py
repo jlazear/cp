ceeb566b27771c7cb7f340e96f082f6c77889fcb
jlazear <jlazear@gmail.com>
2014-10-24 16:18:34 -0400

    Working on installation script.

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='CP',
       py_modules=['cp',],
       packages=['gui'],
       install_requires=['pyoscope==1.0'],
-      dependency_links=['https://github.com/jlazear/pyoscope/archive/master'
-                        '.zip#egg=pyoscope-1.0'],
+      dependency_links=['https://github.com/jlazear/pyoscope/archive'
+                        '/e2ab13f8764b5a64f928884c48c9e963eb57a1fa.zip#egg'
+                        '=pyoscope-1.0']
       )
