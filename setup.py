2eff04c516b980fee35d48e13847905c96cb0329
jlazear <jlazear@gmail.com>
2014-10-24 16:10:03 -0400

    Working on installation script.

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup
 
 setup(name='CP',
       version='1.0',
-      description='Automatic GUI-maker',
+      description='Automatic GUI-maker for hardware interface classes',
       author='Justin Lazear',
       author_email='jlazear@gmail.com',
-      url='http://www.github.com/jlazear/cp',
+      url='https://github.com/jlazear/cp',
       py_modules=['cp',],
       packages=['gui'],
       install_requires=['pyoscope>=1.0'],
-      dependency_links=['https://github.com/jlazear/pyoscope/tarball/master']
+      dependency_links=['https://github.com/jlazear/pyoscope/archive/master'
+                        '.zip'],
       )
